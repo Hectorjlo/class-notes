5a28a7ea2078d2bb7978c5953be90b37e7f5c7fd
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-03-21 13:08:04 -0600

    Add a script to sum three user-input numbers

---
+++ cycles/suma.py
@@ -0,0 +1,8 @@
+numbers_lst = list(map(int, (input("Enter 3 numbers separated by spaces: ").split())))
+
+sum_ = sum(numbers_lst)
+print(sum_)
+
+# nums = [1, 2, 3]
+# add_nums = sum(nums)
+# print(add_nums)
