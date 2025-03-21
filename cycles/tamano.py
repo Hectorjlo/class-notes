0e46301ea67002c6145124f140d852a012e2c199
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-03-21 13:42:43 -0600

    Add genes.gff file and script to calculate gene sizes

---
+++ cycles/tamano.py
@@ -0,0 +1,5 @@
+with open("genes.gff") as file:
+    for line in file:
+        columns = (line.strip()).split("\t")
+        size = int(columns[4]) - int(columns[3]) + 1 # The subtraction take -1 nulceotide so we add 1
+        print(f"THe gene size is: {size}")
