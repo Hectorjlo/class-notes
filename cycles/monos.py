7c1305cface0cdf727d59b14f5bfeea54175ee7a
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-03-21 12:34:18 -0600

    Add iteration example for a list of apes

---
+++ cycles/monos.py
@@ -0,0 +1,9 @@
+apes = ["Pongo pygmaeus", "Pan troglodytes", "Gorilla gorilla"]
+# apes will move through the list of apes, one at a time
+for ape in apes:
+    name_length = len(ape)
+    # first_letter will be the first letter of the current ape, because ape is the iteration variable
+    first_letter = ape[0]
+    print(f"{ape} is an ape. Its name starts with {first_letter}")
+    print(f"Its name has {name_length} letters")
+
