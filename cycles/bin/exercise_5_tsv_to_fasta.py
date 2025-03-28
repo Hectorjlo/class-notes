0a33b5939d19af24e19cc387a3076977da335beb
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-03-28 12:49:13 -0600

    Creating the right folders

---
+++ cycles/bin/exercise_5_tsv_to_fasta.py
@@ -0,0 +1,13 @@
+with open("cycles/dna_sequences.txt", "r") as input_file, open("cycles/dna_sequences.fa", "w") as output_file:
+    for line in input_file:
+        columns = (line.strip()).split("\t")
+        
+
+        output_file.write(f">{columns[0]}\n")  
+        output_file.write(f"{columns[1].upper()}\n")  
+    
+    # or
+
+    # for line in input_file:
+    #    ids, sequence = line.strip().split("\t")
+    #    output_file.write(f">{ids}\n{sequence.upper()}\n")
