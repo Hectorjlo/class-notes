c9696ce2397b230806594eb5cdc42a5784a3c31b
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-04-01 15:00:35 -0600

    Fixing the script with using a f string to better clarity

--- cycles/bin/exercise_13_ATCG_counter.py
+++ cycles/bin/exercise_13_ATCG_counter.py
@@ -1,5 +1,5 @@
 sequences = input("Enter a DNA sequences separeted by comas: ").split(",")
 
-counter = [(f"(sequence.strip()).count('A'), (sequence.strip()).count('T'), (sequence.strip()).count('C'), {(sequence.strip()).count('G')}") for sequence in sequences]
+counter = [f"A: {sequence.strip().count('A')}, T: {sequence.strip().count('T')}, C: {sequence.strip().count('C')}, G: {sequence.strip().count('G')}" for sequence in sequences]
 
 print(counter)
