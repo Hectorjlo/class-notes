caf7509d1ad80080a279e44b22ed287c1cf86bca
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-04-03 17:02:00 -0600

    Reorganize exercise files and minnor fixes on comments on each file

--- src/exercises/3_sequences_counter_seq.py
+++ src/exercises/3_sequences_counter_seq.py
@@ -1,10 +1,10 @@
-# Script for counting each character of a file, not taking the lines starting with ">"
+# Script for counting each character of a file, not taking the lines starting with ">" but their contents
 data_file = "data/exercises/fasta_sequence.txt"
 
 with open(data_file, "r") as input_file:
     lines = input_file.readlines()
 
 filtered_lines = [line for line in lines if line.startswith(">")]
-print(len(filtered_lines))
+# print(len(filtered_lines))
 print(f"Total number of sequences: {len(filtered_lines)}")
     
