32474d08ded751674e3cbcf3b23b82b4547f0247
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-04-25 12:45:14 -0600

    Add a delimitator for maximum number of decimals

--- src/funciones/3_at_counter.py
+++ src/funciones/3_at_counter.py
@@ -5,7 +5,7 @@ def at_counter(dna):
     a_count = dna.count("A")
     t_count = dna.count("T")
     at_content = ((a_count + t_count) / lenght)
-    print(f"AT content is {at_content}% of the sequence")
+    print(f"AT content is {at_content:.2f}% of the sequence")
 
 at_counter("atatatata")
 at_counter("CGAGCAGTACG")
