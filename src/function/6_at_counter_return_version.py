ff76d071e078e4f2ae3ffa20318d19acfe200097
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-04-27 01:04:15 -0600

    at counter rebuild

--- src/function/6_at_counter_return_version.py
+++ src/function/6_at_counter_return_version.py
@@ -1,4 +1,15 @@
-def at_counter(dna, significant_figures):
+def at_counter(dna, significant_figures=2):
+    """
+    Calculates the AT content of a given sequence of DNA,
+    it rounds up of a precise number of significant figures
+
+    Parameters:
+    dna (str): Sequence of DNA (ex. 'ATGCGC')
+    significant_figures (int, opcional): number of significant figures (default = 2)
+
+    Return:
+    float: Content AT round up
+    """
     dna = dna.upper().strip()
     lenght = len(dna)
     a_count = dna.count("A")
@@ -14,3 +25,8 @@ print(result)
 
 result = at_counter(dna="ATCGATCGATCGACG", significant_figures=6)
 print(result)
+
+# TESTING OF AT CONTENT
+assert at_counter("ATCG", 1) == 0.5
+
+at_counter()
