d343c08e0d5ad8a086c34f7601315c93bed3c175
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-05-02 13:52:21 -0600

    Modules created, and tested

--- src/modules/analizator.py
+++ src/modules/analizator.py
@@ -35,24 +35,24 @@ from validator import validate_sequence
 
 
 # --- Inicio del programa ---
-ruta_entrada = "data/secuencias.fasta"
-ruta_salida = "output/resultados.tsv"
+input_path = "data\modules\secuencias.fasta"
+output_path = "results/modules/resultados.tsv"
 
-secuencias = read_fasta(ruta_entrada)
+sequences = read_fasta(input_path)
 
-if not secuencias:
-    print("[INFO] No se procesó ninguna secuencia.")
+if not sequences:
+    print("[INFO] No sequence was processed")
 else:
-    resultados = {}
-    for gen_id, sec in secuencias.items():
+    results = {}
+    for gen_id, sec in sequences.items():
         if not validate_sequence(sec):
-            print(f"[ADVERTENCIA] La secuencia del gen '{gen_id}' contiene bases no válidas y será omitida.")
+            print(f"[ADVERTENCIA] The sequence of the gene '{gen_id}' contains no valid bases and would be ommited.")
             continue
 
         bases = base_counter(sec)
         gc = calculate_gc_content(sec)
-        resultados[gen_id] = {
-            "longitud": len(sec),
+        results[gen_id] = {
+            "lenght": len(sec),
             "A": bases["A"],
             "T": bases["T"],
             "G": bases["G"],
@@ -60,14 +60,14 @@ else:
             "GC%": gc
         }
 
-    if resultados:
-        export_tsv(resultados, ruta_salida)
-        print(f"[OK] Resultados guardados en: {ruta_salida}")
+    if results:
+        export_tsv(results, output_path)
+        print(f"[OK] Results saved in: {output_path}")
     else:
-        print("[INFO] No se generaron resultados válidos para exportar.")
+        print("[INFO] Not valid results were generated hence were not exported.")
 
 # --- Pruebas simples con assert para verificar funciones clave ---
-assert base_counter("ATGC") == {"A": 1, "T": 1, "G": 1, "C": 1}, "Error en contar_bases()"
-assert calculate_gc_content("ATGC") == 50.0, "Error en calcular_gc()"
-assert validate_sequence("ATGC") is True, "Error en validar_secuencia() con secuencia válida"
-assert validate_sequence("ATGN") is False, "Error en validar_secuencia() con caracteres inválidos"
+assert base_counter("ATGC") == {"A": 1, "T": 1, "G": 1, "C": 1}, "Error in base_counter()"
+assert calculate_gc_content("ATGC") == 50.0, "Error in calculate_gc_content()"
+assert validate_sequence("ATGC") is True, "Error in validate_sequence() with invalid sequence"
+assert validate_sequence("ATGN") is False, "Error in validate_sequence() with invalid characters"
