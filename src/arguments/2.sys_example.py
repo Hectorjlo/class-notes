56d97f02123667c35be31277b85eb7057a1d39f8
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-05-23 12:52:38 -0600

    Error handeling for sum

--- src/arguments/2.sys_example.py
+++ src/arguments/2.sys_example.py
@@ -1,6 +1,12 @@
 import sys
 
-if len(sys.argv) > 1:
-    a = float(sys.argv[1])
-    b = float(sys.argv[2])
-    print(f'The sum is: {a + b}')
+try:
+    if len(sys.argv) == 3:
+        a = float(sys.argv[1])
+        b = float(sys.argv[2])
+        print(f'The sum is: {a + b}')
+
+    else:
+        print('Two values are required')
+except Exception:
+    print('Only numbers are permited')
